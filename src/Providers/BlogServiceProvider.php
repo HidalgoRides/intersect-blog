8471ac2f735fafcd92e5f2159abb5f5876e6de57
Tim Kipp <timkipp.22.developer@gmail.com>
2019-08-24 13:33:40 -0700

    Added binding for BlogService class in the BlogServiceProvider

--- src/Providers/BlogServiceProvider.php
+++ src/Providers/BlogServiceProvider.php
@@ -2,12 +2,15 @@
 
 namespace Intersect\Blog\Providers;
 
+use Intersect\Blog\Services\BlogService;
 use Intersect\Core\Providers\ServiceProvider;
 
 class BlogServiceProvider extends ServiceProvider {
 
     public function init()
     {
+        $this->container->bind(BlogService::class, BlogService::class);
+        
         $this->container->migrationPath(dirname(dirname(__FILE__)) . '/Migrations');
     }
 
